6ba3ca1672540345ec630c6acfde91da516c26f8
Amrit <amritds18@gmail.com>
2020-05-12 06:13:45 -0700

    fix runner

--- code/example_runner.py
+++ code/example_runner.py
@@ -2,9 +2,14 @@ import numpy as np
 from PIL import Image
 from main_sampler import main_sampler
 
-generated_images = main_sampler(['A man standing in a field','A girl standing in the ocean'],0)
+gpu_id = 0
 
-for i in len(generated_images):
+sentences = ['A man standing in a field',
+             'A girl standing in the ocean']
+
+generated_images = main_sampler(sentences, gpu_id)
+
+for i in range(len(generated_images)):
     im = Image.fromarray(generated_images[i])
     fullpath = './'+str(i)+'_.png'
     im.save(fullpath)
